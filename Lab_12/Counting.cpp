e81f3790b6c1072a881ace29676c7bad4ac85423
samgraler <samgraler@gmail.com>
2023-04-18 15:29:42 -0400

    Update Counting.cpp

--- Lab_12/Counting.cpp
+++ Lab_12/Counting.cpp
@@ -1,23 +1,27 @@
 #include "Counting.h"
 
-void CountingSort(int* arr, int n) {
-	int i, j;
-	int count[100001] = {0};
-	int temp[100000];
-	for (i = 0; i < n; i++)
+void CountingSort(int* arr, int n)
+{
+	int* temp = new int[n];
+	int* count = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		count[i] = 0;
+	}
+	for (int i = 0; i < n; i++)
 	{
 		count[arr[i]]++;
 	}
-	for (i = 1; i <= 100000; i++)
+	for (int i = 1; i < n; i++)
 	{
 		count[i] += count[i - 1];
 	}
-	for (i = n - 1; i >= 0; i--)
+	for (int i = n - 1; i >= 0; i--)
 	{
 		temp[count[arr[i]] - 1] = arr[i];
 		count[arr[i]]--;
 	}
-	for (i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		arr[i] = temp[i];
 	}
