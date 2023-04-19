463e09ad377957532bbb1c23a72321979ade575c
randalhucker <94146736+randalhucker@users.noreply.github.com>
2023-04-19 12:29:43 -0400

    Update Counting.cpp

--- Lab_12/Counting.cpp
+++ Lab_12/Counting.cpp
@@ -40,37 +40,26 @@ void CountingSort(int* arr, int n)
 
 void CountingSort(int* arr, int n, int exp)
 {
-	int *count, *temp;
-	int max = arr[0];
-	for (int i = 1; i < n; i++) // find max value in array
-	{
-		if (arr[i] > max)
-		{
-			max = arr[i];
-		}
-	}
-	count = new int[10]; // creates a count array for each value in the original array
-	temp = new int[n];
-	for (int i = 0; i <= 10; i++) // initialize count array to all 0's
-	{
-		count[i] = 0;
-	}
-	for (int i = 0; i < n; i++) // gets counts 
-	{
-		count[(arr[i] / exp) % 10]++;
-	}
-	for (int i = 1; i <= max; i++) // get cummulative counts
-	{
-		count[i] += count[i - 1];
-	}
-	for (int i = 0; i < n; i++) // create sorted array
-	{
-		temp[count[(arr[i] / exp) % 10] - 1] = arr[i];
-		count[(arr[i] / exp) % 10]--;
-	}
-	for (int i = 0; i < n; i++) // copy sorted array to original array
-	{
-		arr[i] = temp[i];
-	}
-	delete[] temp;
+    int output[n]; // output array
+    int i, count[10] = { 0 };
+ 
+    // Store count of occurrences in count[]
+    for (i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+ 
+    // Change count[i] so that count[i] now contains actual
+    //  position of this digit in output[]
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+ 
+    // Build the output array
+    for (i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+ 
+    // Copy the output array to arr[], so that arr[] now
+    // contains sorted numbers according to current digit
+    for (i = 0; i < n; i++)
+        arr[i] = output[i];
 }
