1b530a0ed2c1f175ca5bf7e13e5f9e8c45f04383
samgraler <samgraler@gmail.com>
2023-04-20 12:51:23 -0400

    final commit

--- Lab_12/Task1.cpp
+++ Lab_12/Task1.cpp
@@ -18,61 +18,58 @@
 using namespace std;
 typedef chrono::high_resolution_clock Clock;
 
-void MakeArrays(int* arr, int choice)
+void MakeArrays(int* arr, int seed, int choice)
 {
-	std::random_device rd;
-	std::mt19937 mt(rd());
+	std::mt19937 mt(seed); // allows for random numbers to be generated above the typical rand max
 	std::uniform_real_distribution<double> dist;
-	// populate given array with random numbers accoring to the size of the array (indicated by choice)
-	if (choice == 0)
-		std::uniform_real_distribution<double> dist(1, std::nextafter(20, DBL_MAX));
-	else if (choice == 1)
-		std::uniform_real_distribution<double> dist(1, std::nextafter(200, DBL_MAX));
-	else if (choice == 2)
-		std::uniform_real_distribution<double> dist(1, std::nextafter(1000, DBL_MAX));
-	else if (choice == 3)
-		std::uniform_real_distribution<double> dist(1, std::nextafter(10000, DBL_MAX));
-	else if (choice == 4)
-		std::uniform_real_distribution<double> dist(1, std::nextafter(50000, DBL_MAX));
-	else
-		std::uniform_real_distribution<double> dist(1, std::nextafter(200000, DBL_MAX));
-	
+
 	switch(choice){
 		case 0:
 			for (int i = 0; i < 10; i++)
 			{
 				double x = dist(mt);
-				arr[i] = int(x);
+				x *= 20;
+				arr[i] = int(x)+1;
 			}
 			break;
 		case 1:
 			for (int i = 0; i < 100; i++)
 			{
-				arr[i] = int(dist(mt));
+				double x = dist(mt);
+				x *= 200;
+				arr[i] = int(x)+1;
 			}
 			break;
 		case 2:
 			for (int i = 0; i < 500; i++)
 			{
-				arr[i] = int(dist(mt));
+				double x = dist(mt);
+				x *= 1000;
+				arr[i] = int(x)+1;
 			}
 			break;
 		case 3:
 			for (int i = 0; i < 5000; i++)
 			{
-				arr[i] = int(dist(mt));
+				double x = dist(mt);
+				x *= 10000;
+				arr[i] = int(x)+1;
 			}
 			break;
 		case 4:
 			for (int i = 0; i < 25000; i++)
 			{
-				arr[i] = int(dist(mt));
+				double x = dist(mt);
+				x *= 50000;
+				arr[i] = int(x)+1;
 			}
 			break;
 		case 5:
 			for (int i = 0; i < 100000; i++)
 			{
-				arr[i] = int(dist(mt));
+				double x = dist(mt);
+				x *= 100000;
+				arr[i] = int(x)+1;
 			}
 			break;
 		default:
@@ -87,7 +84,7 @@ void PrintArray(int *arr, int n)
 	{
 		cout << arr[i] << " ";
 	}
-	
+	cout << endl;
 }
 
 // print the 2d array that holds the times
@@ -237,11 +234,11 @@ void main1()
 
 	long long results[7][6] = { 0 };
 
-	int seed = time(NULL);
+	int seed = 0;
 	long long averagetime = 0;
 
 	// loop through each array size and call bubble sort 10 times on each size to get an average time for each size
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 6; i++)
 	{
 		averagetime = 0;
 		switch (i)
@@ -249,59 +246,71 @@ void main1()
 			case 0:
 				for (int i = 0; i < 11; i++) // each for loop opertes in the same format
 				{
-					MakeArrays(arr1, 0); // populate the array with random values to be sorted
+					MakeArrays(arr1, seed, 0); // populate the array with random values to be sorted
 					averagetime += Bubble(arr1, 10); // call the respective sort function one time and add the time to the running count
+					seed++;
 				}
 				results[0][0] = averagetime / 10; // calculate the average and store it in a 2D array
+				seed = 0;
 				break;
 			case 1:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr2, 1);
+					MakeArrays(arr2, seed, 1);
 					averagetime += Bubble(arr2, 100);
+					seed++;
 				}
 				results[0][1] = averagetime / 10;
+				seed = 0;
 				break;
 			case 2:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr3, 2);
+					MakeArrays(arr3, seed, 2);
 					averagetime += Bubble(arr3, 500);
+					seed++;
 				}
 				results[0][2] = averagetime / 10;
+				seed = 0;
 				break;
 			case 3:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr4, 3);
+					MakeArrays(arr4, seed, 3);
 					averagetime += Bubble(arr4, 5000);
+					seed++;
 				}
 				results[0][3] = averagetime / 10;
+				seed = 0;
 				break;
 			case 4:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr5, 4);
+					MakeArrays(arr5, seed, 4);
 					averagetime += Bubble(arr5, 25000);
+					seed++;
 				}
 				results[0][4] = averagetime / 10;
+				seed = 0;
 				break;
 			case 5:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr6, 5);
+					MakeArrays(arr6, seed, 5);
 					averagetime += Bubble(arr6, 100000);
+					seed++;
 				}
 				results[0][5] = averagetime / 10;
+				seed = 0;
 				break;
 			default:
 				cout << "Invalid Array" << endl;
 				break;
 		}
 	}
-
+	cout << endl;
 	// loop through each array size and call insertion sort 10 times on each size to get an average time for each size
-	for (int i = 0; i < 3; i++) 
+	for (int i = 0; i < 6; i++) 
 	{
 		averagetime = 0;
 		switch (i)
@@ -309,57 +318,69 @@ void main1()
 		case 0:
 			for (int i = 0; i < 11; i++)
 			{
-				MakeArrays(arr1, 0);
+				MakeArrays(arr1, seed, 0);
 				averagetime += Insertion(arr1, 10);
+				seed++;
 			}
 			results[1][0] = averagetime / 10;
+			seed = 0;
 			break;
 		case 1:
 			for (int i = 0; i < 11; i++)
 			{
-				MakeArrays(arr2, 1);
+				MakeArrays(arr2, seed, 1);
 				averagetime += Insertion(arr2, 100);
+				seed++;
 			}
 			results[1][1] = averagetime / 10;
+			seed = 0;
 			break;
 		case 2:
 			for (int i = 0; i < 11; i++)
 			{
-				MakeArrays(arr3, 2);
+				MakeArrays(arr3, seed, 2);
 				averagetime += Insertion(arr3, 500);
+				seed++;
 			}
 			results[1][2] = averagetime / 10;
+			seed = 0;
 			break;
 		case 3:
 			for (int i = 0; i < 11; i++)
 			{
-				MakeArrays(arr4, 3);
+				MakeArrays(arr4, seed, 3);
 				averagetime += Insertion(arr4, 5000);
+				seed++;
 			}
 			results[1][3] = averagetime / 10;
+			seed = 0;
 			break;
 		case 4:
 			for (int i = 0; i < 11; i++)
 			{
-				MakeArrays(arr5, 4);
+				MakeArrays(arr5, seed, 4);
 				averagetime += Insertion(arr5, 25000);
+				seed++;
 			}
 			results[1][4] = averagetime / 10;
+			seed = 0;
 			break;
 		case 5:
 			for (int i = 0; i < 11; i++)
 			{
-				MakeArrays(arr6, 5);
+				MakeArrays(arr6, seed, 5);
 				averagetime += Insertion(arr6, 100000);
+				seed++;
 			}
 			results[1][5] = averagetime / 10;
+			seed = 0;
 			break;
 		default:
 			cout << "Invalid Array" << endl;
 			break;
 		}
 	}
-	
+
 	// loop through each array size and call quick sort 10 times on each size to get an average time for each size
 	for (int i = 0; i < 6; i++)
 	{
@@ -369,50 +390,62 @@ void main1()
 			case 0:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr1, 0);
+					MakeArrays(arr1, seed, 0);
 					averagetime += Quick(arr1, 10);
+					seed++;
 				}
 				results[2][0] = averagetime / 10;
+				seed = 0;
 				break;
 			case 1:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr2, 1);
+					MakeArrays(arr2, seed, 1);
 					averagetime += Quick(arr2, 100);
+					seed++;
 				}
 				results[2][1] = averagetime / 10;
+				seed = 0;
 				break;
 			case 2:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr3, 2);
+					MakeArrays(arr3, seed, 2);
 					averagetime += Quick(arr3, 500);
+					seed++;
 				}
 				results[2][2] = averagetime / 10;
+				seed = 0;
 				break;
 			case 3:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr4, 3);
+					MakeArrays(arr4, seed, 3);
 					averagetime += Quick(arr4, 5000);
+					seed++;
 				}
 				results[2][3] = averagetime / 10;
+				seed = 0;
 				break;
 			case 4:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr5, 4);
+					MakeArrays(arr5, seed, 4);
 					averagetime += Quick(arr5, 25000);
+					seed++;
 				}
 				results[2][4] = averagetime / 10;
+				seed = 0;
 				break;
 			case 5:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr6, 5);
+					MakeArrays(arr6, seed, 5);
 					averagetime += Quick(arr6, 100000);
+					seed++;
 				}
 				results[2][5] = averagetime / 10;
+				seed = 0;
 				break;
 			default:
 				cout << "Invalid Array" << endl;
@@ -429,50 +462,62 @@ void main1()
 		case 0:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr1, 0);
+					MakeArrays(arr1, seed, 0);
 					averagetime += Merge(arr1, 10);
+					seed++;
 				}
 				results[3][0] = averagetime / 10;
+				seed = 0;
 				break;
 			case 1:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr2, 1);
+					MakeArrays(arr2, seed, 1);
 					averagetime += Merge(arr2, 100);
+					seed++;
 				}
 				results[3][1] = averagetime / 10;
+				seed = 0;
 				break;
 			case 2:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr3, 2);
+					MakeArrays(arr3, seed, 2);
 					averagetime += Merge(arr3, 500);
+					seed++;
 				}
 				results[3][2] = averagetime / 10;
+				seed = 0;
 				break;
 			case 3:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr4, 3);
+					MakeArrays(arr4, seed, 3);
 					averagetime += Merge(arr4, 5000);
+					seed++;
 				}
 				results[3][3] = averagetime / 10;
+				seed = 0;
 				break;
 			case 4:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr5, 4);
+					MakeArrays(arr5, seed, 4);
 					averagetime += Merge(arr5, 25000);
+					seed++;
 				}
 				results[3][4] = averagetime / 10;
+				seed = 0;
 				break;
 			case 5:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr6, 5);
+					MakeArrays(arr6, seed, 5);
 					averagetime += Merge(arr6, 100000);
+					seed++;
 				}
 				results[3][5] = averagetime / 10;
+				seed = 0;
 				break;
 			default:
 				cout << "Invalid Array" << endl;
@@ -488,50 +533,62 @@ void main1()
 		case 0:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr1, 0);
+					MakeArrays(arr1, seed, 0);
 					averagetime += Count(arr1, 10);
+					seed++;
 				}
 				results[4][0] = averagetime / 10;
+				seed = 0;
 				break;
 			case 1:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr2, 1);
+					MakeArrays(arr2, seed, 1);
 					averagetime += Count(arr2, 100);
+					seed++;
 				}
 				results[4][1] = averagetime / 10;
+				seed = 0;
 				break;
 			case 2:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr3, 2);
+					MakeArrays(arr3, seed, 2);
 					averagetime += Count(arr3, 500);
+					seed++;
 				}
 				results[4][2] = averagetime / 10;
+				seed = 0;
 				break;
 			case 3:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr4, 3);
+					MakeArrays(arr4, seed, 3);
 					averagetime += Count(arr4, 5000);
+					seed++;
 				}
 				results[4][3] = averagetime / 10;
+				seed = 0;
 				break;
 			case 4:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr5, 4);
+					MakeArrays(arr5, seed, 4);
 					averagetime += Count(arr5, 25000);
+					seed++;
 				}
 				results[4][4] = averagetime / 10;
+				seed = 0;
 				break;
 			case 5:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr6, 5);
+					MakeArrays(arr6, seed, 5);
 					averagetime += Count(arr6, 100000);
+					seed++;
 				}
 				results[4][5] = averagetime / 10;
+				seed = 0;
 				break;
 			default:
 				cout << "Invalid Array" << endl;
@@ -548,50 +605,62 @@ void main1()
 		case 0:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr1, 0);
+					MakeArrays(arr1, seed, 0);
 					averagetime += Radix(arr1, 10);
+					seed++;
 				}
 				results[5][0] = averagetime / 10;
+				seed = 0;
 				break;
 			case 1:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr2, 1);
+					MakeArrays(arr2, seed, 1);
 					averagetime += Radix(arr2, 100);
+					seed++;
 				}
 				results[5][1] = averagetime / 10;
+				seed = 0;
 				break;
 			case 2:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr3, 2);
+					MakeArrays(arr3, seed, 2);
 					averagetime += Radix(arr3, 500);
+					seed++;
 				}
 				results[5][2] = averagetime / 10;
+				seed = 0;
 				break;
 			case 3:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr4, 3);
+					MakeArrays(arr4, seed, 3);
 					averagetime += Radix(arr4, 5000);
+					seed++;
 				}
 				results[5][3] = averagetime / 10;
+				seed = 0;
 				break;
 			case 4:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr5, 4);
+					MakeArrays(arr5, seed, 4);
 					averagetime += Radix(arr5, 25000);
+					seed++;
 				}
 				results[5][4] = averagetime / 10;
+				seed = 0;
 				break;
 			case 5:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr6, 5);
+					MakeArrays(arr6, seed, 5);
 					averagetime += Radix(arr6, 100000);
+					seed++;
 				}
 				results[5][5] = averagetime / 10;
+				seed = 0;
 				break;
 			default:
 				cout << "Invalid Array" << endl;
@@ -607,50 +676,62 @@ void main1()
 		case 0:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr1, 0);
+					MakeArrays(arr1, seed, 0);
 					averagetime += Heap(arr1, 10);
+					seed++;
 				}
 				results[6][0] = averagetime / 10;
+				seed = 0;
 				break;
 			case 1:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr2, 1);
+					MakeArrays(arr2, seed, 1);
 					averagetime += Heap(arr2, 100);
+					seed++;
 				}
 				results[6][1] = averagetime / 10;
+				seed = 0;
 				break;
 			case 2:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr3, 2);
+					MakeArrays(arr3, seed, 2);
 					averagetime += Heap(arr3, 500);
+					seed++;
 				}
 				results[6][2] = averagetime / 10;
+				seed = 0;
 				break;
 			case 3:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr4, 3);
+					MakeArrays(arr4, seed, 3);
 					averagetime += Heap(arr4, 5000);
+					seed++;
 				}
 				results[6][3] = averagetime / 10;
+				seed = 0;
 				break;
 			case 4:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr5, 4);
+					MakeArrays(arr5, seed, 4);
 					averagetime += Heap(arr5, 25000);
+					seed++;
 				}
 				results[6][4] = averagetime / 10;
+				seed = 0;
 				break;
 			case 5:
 				for (int i = 0; i < 11; i++)
 				{
-					MakeArrays(arr6, 5);
+					MakeArrays(arr6, seed, 5);
 					averagetime += Heap(arr6, 100000);
+					seed++;
 				}
 				results[6][5] = averagetime / 10;
+				seed = 0;
 				break;
 			default:
 				cout << "Invalid Array" << endl;
